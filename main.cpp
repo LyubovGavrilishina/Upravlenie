24ead9aca1189da51c7067ff70ee537d07a52b38
LyubovGavrilishina <80360240+LyubovGavrilishina@users.noreply.github.com>
2021-03-14 03:21:12 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+   #include <iostream>
+using namespace std;
+int main()
+{
+    double A=0;
+    double B=0;
+    cout<<"Введите стоимость покупки(для выхода введите любую букву): ";
+    
+    while(cin>>A)
+{
+        if(A<500.0){
+            B=0;
+            cout<<"Стоимость со скидкой: "<<(A-B)<<endl;}else
+        if(A<1000.0){
+            B=A*0.03;
+                cout<<"Стоимость со скидкой: "<<(A-B)<<endl;}else{
+            B=A*0.05;
+                cout<<"Стоимость со скидкой: "<<(A-B)<<endl;}
+}
+    cout<<"Пока\n";
+    return 0;
+}
